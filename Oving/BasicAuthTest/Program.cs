731dcf5dae5da59690706687e91552d3208681f4
Adrian Holthe <56269370+Holt-e@users.noreply.github.com>
2024-06-13 14:45:01 +0200

    sure

---
+++ Oving/BasicAuthTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+var url = "https://google.com";
+
+var username = "sap";
+var password = "2ewqeq";
+
+var client = new HttpClient();
+client.BaseAddress = new Uri(url);
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+    Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}")));
+
+var response = await client.GetAsync(url);
+
+Console.WriteLine(response.StatusCode);
+
